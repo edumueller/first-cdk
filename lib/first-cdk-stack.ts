717573404ca1afce8f849f82d182fe253f47984b
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-26 22:13:16 -0300

    add cfn parameter duration

--- lib/first-cdk-stack.ts
+++ lib/first-cdk-stack.ts
@@ -1,14 +1,20 @@
 import * as cdk from "@aws-cdk/core";
 import { Bucket } from "@aws-cdk/aws-s3";
-import { CfnOutput, Duration } from "@aws-cdk/core";
+import { CfnOutput, Duration, CfnParameter } from "@aws-cdk/core";
 export class FirstCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const duration = new CfnParameter(this, "duration", {
+      type: "Number",
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+    });
 
     const myBucket = new Bucket(this, "someBucket", {
       lifecycleRules: [
         {
-          expiration: Duration.days(5),
+          expiration: Duration.days(duration.valueAsNumber),
         },
       ],
     });
