45e09cd96141bdc40a912fabd14e00467de9f3d2
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-26 22:06:14 -0300

    deploy a stack and output bucket name

--- lib/first-cdk-stack.ts
+++ lib/first-cdk-stack.ts
@@ -1,9 +1,20 @@
-import * as cdk from '@aws-cdk/core';
-
+import * as cdk from "@aws-cdk/core";
+import { Bucket } from "@aws-cdk/aws-s3";
+import { CfnOutput, Duration } from "@aws-cdk/core";
 export class FirstCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myBucket = new Bucket(this, "someBucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(5),
+        },
+      ],
+    });
+
+    new CfnOutput(this, "mybucket", {
+      value: myBucket.bucketName,
+    });
   }
 }
